8a565f6a0309ffc7b8185679269a4f7b25a316c4
Xiong <xiongblw0852@hanslaser.com>
2019-07-31 16:31:56 +0800

    添加Excel保存

---
+++ CQ/ExcelService.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CQ
+{
+    class ExcelService
+    {
+        private static ExcelService _intance = null;
+        private static readonly object _lock = new object();
+        public static ExcelService Instance
+        {
+            get
+            {
+                if (_intance == null)
+                {
+                    lock (_lock)
+                    {
+                        if (_intance == null)
+                        {
+                            _intance = new ExcelService();
+                        }
+                    }
+                }
+                return _intance;
+            }
+        }
+
+        private ExcelService()
+        {
+        }
+
+        public void Save(string FileName, ObservableCollection<Model> models)
+        {
+            try
+            {
+                FileStream fs = new FileStream(FileName, FileMode.Create);
+
+                int idx = FileName.LastIndexOf('.');
+                string ext = FileName.Substring(idx + 1);
+                NPOI.SS.UserModel.IWorkbook workbook = null;
+
+                if (ext == "xls")
+                {
+                    workbook = new NPOI.HSSF.UserModel.HSSFWorkbook();
+                }
+                else if (ext == "xlsx")
+                {
+                    workbook = new NPOI.XSSF.UserModel.XSSFWorkbook();
+                }
+                else
+                {
+                    return;
+                }
+
+                var sheet = workbook.CreateSheet();
+                var row0 = sheet.CreateRow(0);
+                row0.CreateCell(0).SetCellValue("序号");
+                row0.CreateCell(1).SetCellValue("条码");
+                row0.CreateCell(2).SetCellValue("AB股流量");
+                row0.CreateCell(3).SetCellValue("A股压力");
+                row0.CreateCell(4).SetCellValue("B股压力");
+                row0.CreateCell(5).SetCellValue("A股流量");
+                row0.CreateCell(6).SetCellValue("B股流量");
+                row0.CreateCell(7).SetCellValue("AB胶比例");
+
+                for (int i = 0; i < models.Count; i++)
+                {
+                    var row = sheet.CreateRow(i + 1);
+                    row.CreateCell(0).SetCellValue(models[i].Id);
+                    row.CreateCell(1).SetCellValue(models[i].QRCode);
+                    row.CreateCell(2).SetCellValue(models[i].ABFlow);
+                    row.CreateCell(3).SetCellValue(models[i].APressure);
+                    row.CreateCell(4).SetCellValue(models[i].BPressure);
+                    row.CreateCell(5).SetCellValue(models[i].AFlow);
+                    row.CreateCell(6).SetCellValue(models[i].BFlow);
+                    row.CreateCell(7).SetCellValue(models[i].ABRatio);
+                }
+                workbook.Write(fs);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
