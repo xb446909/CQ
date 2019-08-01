b9c0f21ce65b3f9a0832962e1a7c4565279a3e6f
ByronXiong <xiong-bin@live.com>
2019-08-01 08:14:38 +0800

    临时提交

--- CQ/SerialPortService.cs
+++ CQ/SerialPortService.cs
@@ -4,6 +4,7 @@ using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace CQ
 {
@@ -35,7 +36,32 @@ namespace CQ
         {
             serialPort = new SerialPort();
             string str = System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
-            string BuadRate = IniService.Instance.ReadIniData("扫码枪", "", "DB2.0", str + "Config.ini");
+            string PortName = IniService.Instance.ReadIniData("扫码枪", "串口号", "COM1", str + "Config.ini");
+            string BaudRate = IniService.Instance.ReadIniData("扫码枪", "波特率", "115200", str + "Config.ini");
+            string Parity = IniService.Instance.ReadIniData("扫码枪", "校验位", "0", str + "Config.ini");
+            string DataBits = IniService.Instance.ReadIniData("扫码枪", "数据位", "8", str + "Config.ini");
+            string StopBits = IniService.Instance.ReadIniData("扫码枪", "停止位", "1", str + "Config.ini");
+
+            serialPort.PortName = PortName;
+            if (int.TryParse(BaudRate, out int nBaudRate) == false)
+            {
+                MessageBox.Show("波特率设置错误!");
+                return;
+            }
+            serialPort.BaudRate = nBaudRate;
+            if (int.TryParse(Parity, out int nParity) == false)
+            {
+                MessageBox.Show("校验位设置错误!");
+                return;
+            }
+            serialPort.Parity = (System.IO.Ports.Parity)nParity;
+            if (int.TryParse(DataBits, out int nDataBits) == false)
+            {
+                MessageBox.Show("数据位设置错误!");
+                return;
+            }
+            serialPort.DataBits = nDataBits;
+
         }
 
         public void OpenSerialPort()
